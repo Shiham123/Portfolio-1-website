50ece6fd96c656fa3934613802fe89168964e4ca
unknown <s36651786@gmail.com>
2022-11-05 12:07:30 +0600

    mobile screen responsive

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ function PageTransition() {
       element.classList.add('active');
     }
   });
+  const themeBtn = document.querySelector('.theme-btn');
+  themeBtn.addEventListener('click', () => {
+    let element = document.body;
+    element.classList.toggle('light-mode');
+  });
 }
 
 PageTransition();
